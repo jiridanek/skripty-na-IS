a66ad3cac9c8ea3629382a669a2a94955b14edc6
xdanek7 <dnk@mail.muni.cz>
2010-11-22 15:41:38 +0100

    par novych getteru pro Predmet a pokus o vkladani predmetu do rozvrhu

--- cgi.py
+++ cgi.py
@@ -3,30 +3,64 @@
 
 import cgi
 import xml.dom.minidom
+
+import re
 #cgitb.enable()
 
+OBDOBI = "podzim2010"
+FAKULTY = {'LF' : 1411 , 'FF' : 1421 , 'PrF' : 1422 , 'FSS' : 1423 , u'PÅ™F' : 1431 , 'FI' : 1433, 'PdF' : 1441 , 'ESF' : 1456 , 'FSpS' : 1451, 'CUS' : 1490}  
+STUDIJNI_MATERIALY="https://is.muni.cz/auth/student/studijni_materialy.pl"
+
 class Predmet:
-	def __init__(self, jmeno="", kod="", id=0, ucebna=("",0), od="", do="", den=""):
+	def __init__(self, jmeno="", kod="", pid=0, ucebna=[("",0)], od="", do="", den=""):
 		self.jmeno = jmeno
 		self.kod = kod
 		self.ucebna = ucebna
 		self.od = od
 		self.do = do
 		self.den = den
-	def to_div():
+		self.pid = pid
+
+		self.base_predmet_url = "https://is.muni.cz/auth/predmety/predmet.pl?id="
+		self.base_ucebna_url="https://is.muni.cz/auth/kontakty/mistnost.pl?id="
+		self.template_materialy_url = "https://is.muni.cz/auth/dok/rfmgr.pl?furl=/el/{fakulta}/{obdobi}/" + self.jmeno + "/" #{predmet}/"
+	def to_div(self):
 		pass
-		
+	def get_predmet_url(self):
+		return self.base_predmet_url + str(self.pid)
+	def get_ucebna_url(self):
+		return map(lambda u:(u[0], self.base_ucebna_url + str(u[1])),self.ucebna)
+	def get_materialy_url(self):
+		import string
+		f = string.Formatter
+		return f.format(template_materialy_url, fakulta = "", obdobi = OBDOBI)
+	def get_odcas_s(self):
+
+		m = re.match("([0-9]*):([0-9]*)", self.od)
+		return int(m.group(1))*60*60 + int(m.group(1))*60
+	def get_docas_s(self):
+
+		m = re.match("([0-9]*):([0-9]*)", self.do)
+		return int(m.group(1))*60*60 + int(m.group(1))*60
+#FIXME TODO
+def dira(radek, predmet):
+	predmety = sorted(radek, key=lambda x:x.get_odcas_s())
+	for index,predmet in enumerate(predmety):
+		if predmet.get_odcas_s() > predmet.get_odcas_s():
+			break
+		not index +1 == length predmety
+
 def group_by(co, podle_ceho):
-	grupy=[]
+	grupy={}
 	grupa=[]
 	for i in sorted(co, key=podle_ceho):
 		if grupa == [] or podle_ceho(grupa[-1]) == podle_ceho(i):
 			grupa.append(i)
 		else:
-			grupy.append(grupa)
+			grupy[podle_ceho(grupa[0])]=grupa
 			grupa = []
 	if not grupa == []:
-		grupy.append(grupa)
+		grupy[podle_ceho(grupa[0])] = grupa
 	return grupy
 
 ###
@@ -43,7 +77,7 @@ print                               # blank line, end of headers
 #form = cgi.FieldStorage()
 #if "xmlko" not in form:
 #    print "<H1>Error</H1>"
-#    print "®ádný rozvrh ke zpracování."
+#    print "Â®Ã¡dnÃ½ rozvrh ke zpracovÃ¡nÃ­."
 
 fajl = open("rozvrh.xml")
 doc = xml.dom.minidom.parseString(fajl.read())
@@ -74,6 +108,6 @@ else:
 		except Exception,e:
 			denid = None
 		
-		p = Predmet(nazev, kod, predmetid, (mistnost, mistnostid), odcas, docas, denid)
+		p = Predmet(nazev, kod, predmetid, [(mistnost, mistnostid)], odcas, docas, denid)
 		predmety.append(p)
 	
